159836b5ca7aea0f39614def9ed924a4d97cdc1d
ahnjungwook <dkswjddnr2@naver.com>
2021-08-08 14:03:25 +0900

    델리게이트 주석 추가

--- Source/TestProject/Util/Delegate.h
+++ Source/TestProject/Util/Delegate.h
@@ -24,10 +24,15 @@ public:
 	}
 
 	Delegate& operator -= (const std::function<void(ARGS...)>& func)
-	{
+	{		
+		// std::function::target 템플릿 멤버 함수를 사용할 때,
+		// 템플릿 클래스의 템플릿 멤버 함수를 호출하게 되면 컴파일러가 타입을 추론하지 못함
+		// 컴파일러에게 템플릿 함수라는 것을 명시적으로 알려주기 위해 template 키워드 사용
 		void (* const* func_ptr)(ARGS...) = func.template target<void(*)(ARGS...)>();
 		const std::size_t func_hash = func.target_type().hash_code();
 
+		// 클래스 멤버 함수나 람다의 경우 해쉬 코드를 비교
+		// <void(*)(ARGS...)> 타입이 아니기 때문에 target을 이용해 함수 포인터를 얻으면 nullptr를 반환함
 		if (nullptr == func_ptr)
 		{
 			for (auto itr = functions.begin(); itr != functions.end(); itr++)
@@ -39,6 +44,8 @@ public:
 				}
 			}
 		}
+		// 그 이외 일반 함수의 경우 같은 포맷의 함수는 같은 해쉬 코드를 반환함
+		// <void(*)(ARGS...)> 타입이기 때문에 함수 포인터로 비교 가능
 		else
 		{
 			for (auto itr = functions.begin(); itr != functions.end(); itr++)
